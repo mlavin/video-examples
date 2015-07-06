0fed9440640fd236f6cc75863870a1ac3038f371
mlavin <markdlavin@gmail.com>
2015-07-06 16:29:03 -0400

    Testing the new stale method along and chaining with existing active filter.

--- domainchecks/tests/test_models.py
+++ domainchecks/tests/test_models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.test import TestCase
+from django.utils.timezone import now
 
 from .. import models
 
@@ -18,6 +21,19 @@ class DomainChecksTestCase(TestCase):
         values.update(kwargs)
         return models.DomainCheck.objects.create(**values)
 
+    def create_check_result(self, **kwargs):
+        """Create a sample check result."""
+        values = {
+            'checked_on': now(),
+            'status_code': 200,
+            'response_time': 0.5,
+            'response_body': 'Ok'
+        }
+        values.update(kwargs)
+        if 'domain_check' not in values:
+            values['domain_check'] = self.create_domain_check()
+        return models.CheckResult.objects.create(**values)
+
     def test_active_manager(self):
         """Active manager should only return active domains."""
         active = self.create_domain_check(is_active=True)
@@ -35,3 +51,42 @@ class DomainChecksTestCase(TestCase):
         result = models.DomainCheck.objects.all().order_by('pk')
         self.assertQuerysetEqual(
             result, [active.pk, inactive.pk], transform=lambda x: x.pk)
+
+    def test_stale_domains(self):
+        """Query for domains which haven't been checked recently."""
+        # Domain with a recent check
+        recent = self.create_domain_check()
+        self.create_check_result(domain_check=recent)
+        # Domain with an old check
+        stale = self.create_domain_check()
+        self.create_check_result(
+            domain_check=stale,
+            checked_on=now() - datetime.timedelta(days=1))
+        # Domain with no checks
+        no_results = self.create_domain_check()
+        result = models.DomainCheck.objects.stale().order_by('pk')
+        self.assertQuerysetEqual(
+            result, [stale.pk, no_results.pk], transform=lambda x: x.pk)
+
+    def test_active_stale_domains(self):
+        """Query only active domains which haven't been checked recently."""
+        # Domain with a recent check
+        recent = self.create_domain_check()
+        self.create_check_result(domain_check=recent)
+        # Domain with an old check
+        stale = self.create_domain_check()
+        self.create_check_result(
+            domain_check=stale,
+            checked_on=now() - datetime.timedelta(days=1))
+        # Inactive domain with an old check
+        stale_inactive = self.create_domain_check(is_active=False)
+        self.create_check_result(
+            domain_check=stale_inactive,
+            checked_on=now() - datetime.timedelta(days=1))
+        # Domain with no checks
+        no_results = self.create_domain_check()
+        # Inactive domain with no checks
+        self.create_domain_check(is_active=False)
+        result = models.DomainCheck.objects.active().stale().order_by('pk')
+        self.assertQuerysetEqual(
+            result, [stale.pk, no_results.pk], transform=lambda x: x.pk)
