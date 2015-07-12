b530313ac1ccd258a5d0da668636bb27e9d6214f
mlavin <markdlavin@gmail.com>
2015-07-12 09:19:13 -0400

    Registering DomainCheck in the admin.

--- domainchecks/admin.py
+++ domainchecks/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.utils.timesince import timesince
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.DomainCheck)
+class DomainCheckAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'domain', 'path', 'protocol', 'method', 'is_active',
+        'status', 'last_checked', )
+    list_filter = ('protocol', 'method', 'is_active', )
+    search_fields = ('domain', )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).status()
+
+    def status(self, obj):
+        return obj.status.title()
+    status.admin_order_field = 'success_rate'
+
+    def last_checked(self, obj):
+        if obj.last_check:
+            return '{} ago'.format(timesince(obj.last_check))
+        else:
+            return 'Never'
+    last_checked.admin_order_field = 'last_check'
