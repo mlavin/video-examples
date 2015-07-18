b7182627d854d990e35364f0d02121a91a8979b1
mlavin <markdlavin@gmail.com>
2015-07-18 13:20:08 -0400

    Add tests for the domain checks admin customizations.

--- domainchecks/tests/test_admin.py
+++ domainchecks/tests/test_admin.py
@@ -1,7 +1,8 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 from django.contrib.admin import site
 from django.test import RequestFactory, TestCase
+from django.utils.timezone import now
 
 from .. import admin, models
 from . import factories
@@ -70,3 +71,68 @@ class StatusListFilterTestCase(TestCase):
         result = list_filter.queryset(request, qs)
         self.assertQuerysetEqual(
             result.order_by('pk'), [good.pk, ], transform=lambda x: x.pk)
+
+
+class DomainCheckAdminTestCase(TestCase):
+    """Customizations to the domain checks admin."""
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.admin = admin.DomainCheckAdmin(models.DomainCheck, site)
+
+    def test_get_queryset(self):
+        """Default queryset should include the status field annotations."""
+        example = factories.create_domain_check()
+        self.assertFalse(hasattr(example, 'status'))
+        self.assertFalse(hasattr(example, 'last_check'))
+        request = self.factory.get('/admin/')
+        result = self.admin.get_queryset(request)
+        self.assertEqual(result.count(), 1)
+        domain = result[0]
+        self.assertTrue(hasattr(domain, 'status'))
+        self.assertTrue(hasattr(domain, 'last_check'))
+
+    def test_format_status(self):
+        """Status field should be title cased."""
+        example = Mock()
+        example.status = 'good'
+        result = self.admin.status(example)
+        self.assertEqual(result, 'Good')
+
+    def test_format_last_check(self):
+        """Last check should be formatted as timesince the current time."""
+        example = Mock()
+        example.last_check = now()
+        result = self.admin.last_checked(example)
+        self.assertEqual(result, '0\xa0minutes ago')
+
+    def test_format_no_last_check(self):
+        """If there is no last check it should display 'Never'."""
+        example = Mock()
+        example.last_check = None
+        result = self.admin.last_checked(example)
+        self.assertEqual(result, 'Never')
+
+    def test_run_check(self):
+        """Run check action should call the run_check model method."""
+        example = Mock()
+        qs = [example, ]
+        request = self.factory.get('/admin/')
+        self.admin.run_check(request, qs)
+        example.run_check.assert_called_with()
+
+    def test_mark_inactive(self):
+        """Selected items should be changed in is_active=False."""
+        selected = factories.create_domain_check()
+        not_selected = factories.create_domain_check()
+        request = self.factory.get('/admin/')
+        qs = self.admin.get_queryset(request).filter(pk=selected.pk)
+        # Mock the contrib.messages middleware
+        with patch.object(self.admin, 'message_user') as mock_message:
+            self.admin.mark_inactive(request, qs)
+            mock_message.assert_called_with(
+                request, '1 domain was made inactive.')
+        selected.refresh_from_db()
+        not_selected.refresh_from_db()
+        self.assertFalse(selected.is_active)
+        self.assertTrue(not_selected.is_active)
