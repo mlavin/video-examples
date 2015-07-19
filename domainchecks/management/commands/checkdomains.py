5bd94bcaeea11cf0903e00f79a1a47329efd4973
mlavin <markdlavin@gmail.com>
2015-07-18 20:41:11 -0400

    Add command line options to the checkdomains command.

--- domainchecks/management/commands/checkdomains.py
+++ domainchecks/management/commands/checkdomains.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.core.management import BaseCommand
 
 from ...models import DomainCheck
@@ -6,15 +8,24 @@ from ...models import DomainCheck
 class Command(BaseCommand):
     help = 'Pings configured domain checks for their current status.'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--minutes', type=int, dest='minutes', default=5,
+            help='Time cutoff from the last check (in minutes).')
+        parser.add_argument(
+            '--timeout', type=int, dest='timeout', default=10,
+            help='Timeout for server response (in seconds).')
+
     def handle(self, *args, **options):
         verbosity = options['verbosity']
         if verbosity > 0:
             self.stdout.write('Refreshing domain statuses\n')
         count = 0
-        for check in DomainCheck.objects.active().stale():
+        cutoff = datetime.timedelta(minutes=options['minutes'])
+        for check in DomainCheck.objects.active().stale(cutoff=cutoff):
             if verbosity > 1:
                 self.stdout.write('Running check {}\n'.format(check))
-            check.run_check()
+            check.run_check(timeout=options['timeout'])
             count += 1
         if verbosity > 0:
             self.stdout.write('{count} domain status{plural} updated\n'.format(
