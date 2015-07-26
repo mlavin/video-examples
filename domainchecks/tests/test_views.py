02fb2aa618ad6ecc59abcd3defe418c3ecac6d53
mlavin <markdlavin@gmail.com>
2015-07-26 12:24:18 -0400

    Tests for JSON view.

--- domainchecks/tests/test_views.py
+++ domainchecks/tests/test_views.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 from django.core.urlresolvers import reverse
 from django.http import Http404
 from django.test import RequestFactory, TestCase
@@ -78,3 +80,77 @@ class StatusDetailViewTestCase(TestCase):
             response = self.client.get(url)
             self.assertContains(response, 'HTTP GET /')
             self.assertContains(response, 'HTTP GET /extra/')
+
+
+class CheckTimelineViewTestCase(TestCase):
+    """JSON list of check results."""
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.view = views.CheckTimeline.as_view()
+
+    def test_no_results(self):
+        """A check with no results should return no results."""
+        check = factories.create_domain_check()
+        url = reverse('status-timeline', kwargs={'check': check.pk})
+        request = self.factory.get(url)
+        response = self.view(request, check=check.pk)
+        self.assertEqual(response.status_code, 200)
+
+    def test_inactive(self):
+        """Inactive checks should 404."""
+        check = factories.create_domain_check(is_active=False)
+        url = reverse('status-timeline', kwargs={'check': check.pk})
+        request = self.factory.get(url)
+        with self.assertRaises(Http404):
+            self.view(request, check=check.pk)
+
+    def test_get_results(self):
+        """Build result dictionary from context."""
+        view = views.CheckTimeline()
+        view.request = Mock()
+        view.args = []
+        view.kwargs = {'check': 1}
+        context = {
+            'page_obj': Mock(object_list=[]),
+            'paginator': Mock(count=0),
+            'object_list': [],
+        }
+        result = view.get_results(context)
+        self.assertEqual(result, {'count': 0, 'results': []})
+
+    def test_render_response(self):
+        """Response data should be returned as JSON."""
+        check = factories.create_domain_check()
+        success = factories.create_check_result(
+            domain_check=check, status_code=200, response_time=0.1)
+        failure = factories.create_check_result(
+            domain_check=check, status_code=404, response_time=0.1)
+        url = reverse('status-timeline', kwargs={'check': check.pk})
+        request = self.factory.get(url)
+        response = self.view(request, check=check.pk)
+        self.assertEqual(response['Content-Type'], 'application/json')
+
+        def convert_date(date):
+            """Convert to ECMA-262 format."""
+            result = date.isoformat()
+            if date.microsecond:
+                result = result[:23] + result[26:]
+            if result.endswith('+00:00'):
+                result = result[:-6] + 'Z'
+            return result
+
+        expected = [
+            {
+                'checked_on': convert_date(failure.checked_on),
+                'status_code': failure.status_code,
+                'response_time': failure.response_time,
+            },
+            {
+                'checked_on': convert_date(success.checked_on),
+                'status_code': success.status_code,
+                'response_time': success.response_time,
+            },
+        ]
+        self.assertJSONEqual(
+            response.content.decode('utf-8'), {'count': 2, 'results': expected})
