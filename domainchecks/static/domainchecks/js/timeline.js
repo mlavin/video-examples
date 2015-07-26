02bd322f379107766edfd6a17550e8ad83dfa991
mlavin <markdlavin@gmail.com>
2015-07-26 12:54:44 -0400

    Rendering graphs of the response times and errors for the status detail page.

---
+++ domainchecks/static/domainchecks/js/timeline.js
@@ -0,0 +1,49 @@
+(function ($) {
+    'use strict';
+
+    var chartOptions = {
+        xaxes: [{
+            show: true,
+            mode: 'time',
+            minTickSize: [1, 'hour'],
+            timezone: 'browser'
+        }],
+        yaxes: [{
+            show: true,
+            position: 'left',
+            min: 0
+        }],
+        legend: {
+            show: false
+        }
+    };
+
+    function renderResponseTimes(elem, data) {
+        var series = data.map(function (item) {
+                return [
+                    Date.parse(item['checked_on']),
+                    item['response_time'] * 1000
+                ];
+            }),
+            errors = data.map(function (item) {
+                return [
+                    Date.parse(item['checked_on']),
+                    item['status_code'] > 299 ? item['response_time'] * 1000 || 0 : null
+                ];
+            });
+        $.plot(elem, [
+                {data: series},
+                {data: errors, points: {show: true}, color: '#c30'}
+            ], chartOptions);
+    }
+
+    $('.charts[data-url]').each(function (row) {
+        var $row = $(this),
+            url = $row.data('url'),
+            responseTime = $row.find('.response-time');
+        $.getJSON(url).done(function (data) {
+            renderResponseTimes(responseTime, data.results);
+        });
+    });
+
+})(jQuery);
