8061c7abed71b8327fb20c6513763f8652a78371
mlavin <markdlavin@gmail.com>
2015-08-01 20:27:13 -0400

    Adjust the front end to allow selecting a few different time frames.

--- domainchecks/static/domainchecks/js/timeline.js
+++ domainchecks/static/domainchecks/js/timeline.js
@@ -22,13 +22,32 @@
             ], chartOptions);
     }
 
+    function dateString(date) {
+        return date.toISOString()
+            .replace('T', ' ')
+            .replace(/\.\d+Z$/, ' ');
+    }
+
     $('.charts[data-url]').each(function (row) {
         var $row = $(this),
             url = $row.data('url'),
-            responseTime = $row.find('.response-time');
-        $.getJSON(url).done(function (data) {
-            renderResponseTimes(responseTime, data.results);
+            responseTime = $row.find('.response-time'),
+            timeFrame = $row.find(':input[name="timeframe"]');
+        timeFrame.on('change', function (e) {
+            var choice = $(this).val(),
+                now = new Date(),
+                start = new Date(),
+                offset = parseInt(choice, 10),
+                params = {
+                    end: dateString(now),
+                };
+            start.setHours(now.getHours() - offset);
+            params.start = dateString(start);
+            $.getJSON(url, params).done(function (data) {
+                renderResponseTimes(responseTime, data.results);
+            });
         });
+        timeFrame.change();
     });
 
 })(jQuery);
