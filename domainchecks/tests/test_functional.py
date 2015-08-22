d965897989039a58a4c71daddbe951cfa54613dd
mlavin <markdlavin@gmail.com>
2015-08-22 12:27:25 -0400

    Adding Selenium as a requirement and adding new functional tests using the PhantomJS driver.

---
+++ domainchecks/tests/test_functional.py
@@ -0,0 +1,88 @@
+import shutil
+import unittest
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from selenium import webdriver
+
+from . import factories
+
+
+@unittest.skipUnless(shutil.which('phantomjs'), 'PhantomJS is not installed.')
+class BrowserTestCase(StaticLiveServerTestCase):
+    """Functional user tests."""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.browser = webdriver.PhantomJS()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super().tearDownClass()
+
+    def setUp(self):
+        self.user = factories.create_user(password='test', is_staff=True)
+        self.domain = factories.create_domain(owner=self.user)
+        self.check = factories.create_domain_check(domain=self.domain)
+        self.login(self.user.username, 'test')
+
+    def tearDown(self):
+        self.logout()
+
+    def get(self, url):
+        self.browser.get('%s%s' % (self.live_server_url, url))
+
+    def login(self, username, password):
+        """Navigate to login page and login."""
+        # FIXME: There is currently no way to login unless you are staff.
+        self.get('/admin/login/')
+        self.browser.find_element_by_name('username').send_keys(username)
+        self.browser.find_element_by_name('password').send_keys(password)
+        self.browser.find_element_by_tag_name('form').submit()
+
+    def logout(self):
+        """Logout current user."""
+        self.get('/admin/logout/')
+
+    def test_view_domains(self):
+        """Login and view user's domains."""
+        self.get('/')
+        domain = self.browser.find_element_by_class_name('domain')
+        name = domain.find_element_by_class_name('name')
+        self.assertEqual(name.text, self.domain.name)
+
+    def test_view_domain_detail(self):
+        """View the status detail for a single domain."""
+        self.get('/domains/{}/'.format(self.domain.name))
+        title = self.browser.find_element_by_tag_name('h1')
+        self.assertEqual(title.text, self.domain.name)
+        check = self.browser.find_element_by_class_name('check')
+        name = check.find_element_by_class_name('name')
+        expected = '{protocol} {method} {path}'.format(
+            protocol=self.check.get_protocol_display(),
+            method=self.check.get_method_display(),
+            path=self.check.path)
+        self.assertEqual(name.text, expected)
+
+    def test_add_domain(self):
+        """Create a new domain for this user."""
+        self.get('/domains/add/')
+        form = self.browser.find_element_by_tag_name('form')
+        form.find_element_by_name('name').send_keys('new.com')
+        form.find_element_by_name('checks-0-path').send_keys('/')
+        form.submit()
+        title = self.browser.find_element_by_tag_name('h1')
+        self.assertEqual(title.text, 'new.com')
+
+    def test_edit_domain(self):
+        """Edit a domain for this user."""
+        self.get('/domains/{}/edit/'.format(self.domain.name))
+        form = self.browser.find_element_by_tag_name('form')
+        name = form.find_element_by_name('name')
+        name.clear()
+        name.send_keys('edit.com')
+        form.submit()
+        title = self.browser.find_element_by_tag_name('h1')
+        self.assertEqual(title.text, 'edit.com')
