c08e7f8aea961d27611b28d4b07c1b3696574a01
mlavin <markdlavin@gmail.com>
2015-08-30 13:13:15 -0400

    Change the default settings module used.

--- statuspage/celery.py
+++ statuspage/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'statuspage.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'statuspage.settings.dev')
 
 from django.conf import settings
 
